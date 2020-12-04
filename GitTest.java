3d0f44a32981448f4dee1481ef51223ef3ec6497
yinkyawt <yinkyawt19@gmail.com>
2020-12-04 01:18:55 -0800

    add for loop

--- GitTest.java
+++ GitTest.java
@@ -1,6 +1,9 @@
 public class GitTest {
     public static void main(String[] args) {
 
-        System.out.println("Git Test");
+        for(int i = 0 ; i < 10 ; i++ ){
+
+            System.out.print(i + " ");
+        }
     }
 }
